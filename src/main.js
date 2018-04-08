e5079bb90349b524371a439a83047001a94f956c
Jimmy <ljh89st@gmail.com>
2018-04-09 01:47:32 +0900

    first commit

---
+++ src/main.js
@@ -0,0 +1,26 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import firebase from 'firebase'
+
+Vue.config.productionTip = false
+
+var config = {
+  apiKey: "AIzaSyDrT5ztdUEjuXKvEK1QMLaB1Vf80GvLcLY",
+  authDomain: "friendspetsitter.firebaseapp.com",
+  databaseURL: "https://friendspetsitter.firebaseio.com",
+  projectId: "friendspetsitter",
+  storageBucket: "friendspetsitter.appspot.com",
+  messagingSenderId: "1031150508085"
+};
+firebase.initializeApp(config)
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  template: '<App/>',
+  components: { App }
+})
